6e87589743c3fcde417496dc21c919def2e6060a
RamtinMoslemi <ramtin4moslemi@gmail.com>
2024-06-04 18:32:52 +0330

    Fix typo

--- MiniJava/src/main/java/parser/Parser.java
+++ MiniJava/src/main/java/parser/Parser.java
@@ -71,7 +71,7 @@ public class Parser {
                         try {
                             cg.semanticFunction(rule.getSemanticAction(), lookAhead);
                         } catch (Exception e) {
-                            Log.print("Code Genetator Error");
+                            Log.print("Code Generator Error");
                         }
                         break;
                     case accept:
