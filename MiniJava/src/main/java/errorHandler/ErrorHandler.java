d891fca838040fce502b5f09d7a741bf67883199
RamtinMoslemi <ramtin4moslemi@gmail.com>
2024-06-04 17:59:23 +0330

    Encapsulate field

--- MiniJava/src/main/java/errorHandler/ErrorHandler.java
+++ MiniJava/src/main/java/errorHandler/ErrorHandler.java
@@ -5,10 +5,14 @@ package errorHandler;
  */
 
 public class ErrorHandler {
-    public static boolean hasError = false;
+    private static boolean hasError = false;
 
     public static void printError(String msg) {
         hasError = true;
         System.out.println(msg);
+    }
+
+    public static boolean getHasError() {
+        return hasError;
     }
 }
